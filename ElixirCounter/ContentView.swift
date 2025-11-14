b3dcfebf057478b6182741c863ca84b63ef87492
Thomas Noone <tnoone@lcs.on.ca>
2025-11-14 09:12:28 -0500

    Initial Commit

---
+++ ElixirCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElixirCounter
+//
+//  Created by Thomas Noone on 2025-11-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElixirCounter/ElixirCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElixirCounterApp.swift
+//  ElixirCounter
+//
+//  Created by Thomas Noone on 2025-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ElixirCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
